e79a3a8659975f05e8a616af05e326ea1c420665
ofcmail <ofcmailkom@gmail.com>
2022-12-15 09:58:18 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,19 @@ public class Main {
     static Employee [] employee = new Employee[10];
 
     public static void makeListOfEmployees() {
-        for (Object empl : employee) {
-            if (employee != null) {
-                System.out.println(empl);
+        for (int i=0;i<employee.length;i++ ) {
+            if (employee[i] == null) {
+              i++;
+            }else{
+                System.out.println(employee[i]);;
             }
         }
     }
     public static void makelistOfEmployeesBySurname() {
         for (int i = 0; i < employee.length; i++) {
-            if (employee[i] != null) {
+            if (employee[i] == null) {
+                i++;
+            } else {
                 System.out.println(employee[i].getFullName());
             }
         }
@@ -19,10 +23,12 @@ public class Main {
         int minSalary = (int) employee[0].getSalary();
         StringBuilder employeeWithMinSalary = new StringBuilder();
         for (int i = 0; i<employee.length; i++){
-            if (employee [i].getSalary()< minSalary && employee[i]!=null) {
-                minSalary = (int) employee[i].getSalary();
-                employeeWithMinSalary.setLength(0);
-                employeeWithMinSalary.append(employee[i].getFullName()).append(" ").append(minSalary);
+            if (employee[i]==null){
+                i++;
+            } else if (employee [i].getSalary()< minSalary && employee[i]!=null) {
+                    minSalary = (int) employee[i].getSalary();
+                    employeeWithMinSalary.setLength(0);
+                    employeeWithMinSalary.append(employee[i].getFullName()).append(" ").append(minSalary);
             }
         }
         return employeeWithMinSalary;
@@ -34,12 +40,14 @@ public class Main {
         int maxSalary = (int) employee[0].getSalary();
         StringBuilder employeeWithMaxSalary = new StringBuilder();
         for (int i = 0; i<employee.length; i++){
-            if (employee[i].getSalary()>maxSalary && employee[i]!=null) {
+            if (employee[i]==null){
+                i++;
+            }else if (employee[i].getSalary()>maxSalary) {
                 maxSalary = (int) employee[i].getSalary();
                 employeeWithMaxSalary.setLength(0);
                 employeeWithMaxSalary.append(employee[i].getFullName()).append(" ").append(maxSalary);;
             }
-        }
+            }
         return employeeWithMaxSalary;
     }
     public static void printMaxSalary (){
@@ -49,7 +57,9 @@ public class Main {
     public static int countAllSalaryPerMonth() {
         int total = 0;
         for (int i = 0; i < employee.length; i++) {
-            if (employee[i] != null) {
+            if (employee[i]==null){
+                i++;
+            } else {
                 total = (int) (total + employee[i].getSalary());
             }
         }
@@ -61,8 +71,9 @@ public class Main {
     public static int countAverageValueOfSalaryPerMonth() {
         int total = 0;
         for (int i = 0; i < employee.length; i++) {
-            if (employee[i] != null) {
-                total = (int) (total + employee[i].getSalary());
+            if (employee[i] == null) {
+                i++;
+            } else {    total = (int) (total + employee[i].getSalary());
             }
         }
         int averageSalary = total/employee.length;
@@ -76,9 +87,11 @@ public class Main {
         StringBuilder newSalar = new StringBuilder();
         int newSalary = 0;
         for (int i=0;i<employee.length;i++) {
-            if (employee[i] != null) {
-                newSalary = (int) (employee[i].getSalary() * indexSalary);
-                newSalar.append("Зарплата после индексации: ").append("\n").append(employee[i].getFullName()).append(" ").append(newSalary).append("\n");
+            if (employee[i] == null) {
+                i++;
+            }else{
+            newSalary = (int) (employee[i].getSalary() * indexSalary);
+            newSalar.append("Зарплата после индексации: ").append("\n").append(employee[i].getFullName()).append(" ").append(newSalary).append("\n");
             }
         }
         return newSalar;
@@ -92,7 +105,7 @@ public class Main {
 
         employee [0] = new Employee("Иванов Иван Иванович", "1", 2222);
         employee [1] = new Employee("Петров Петр Петрович", "2", 1350);
-        employee [2] = new Employee("Николаев Николай Николаевич", "2", 2272);
+        employee [2] = null;
         employee [3] = new Employee("Довыдов Довыд Довыдович", "3", 2552);
         employee [4] = new Employee("Любанова Любовь Ивановна", "3", 2290);
         employee [5] = new Employee("Иванова Иванесса Ивановна", "5", 3246);
